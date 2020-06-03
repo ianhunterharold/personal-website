0432c8447749bbd4bc95fbf017abd63f60da06ab
ianhunterharold <ianhunterharold@gmail.com>
2020-06-03 14:31:13 -0600

    connected styling containers to main and sidebar, wip

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import '../style/sidebarStyle.scss'
 
 
 const SideBar = () => {
 
   return (
-    <>
+    <div className='side-bar-component'>
     "Inside of the sidebar"
-    </>
+    </div>
   )
 }
 
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Component } from 'react';
+import '../style/mainStyle.scss';
 
 class Main extends Component{
 render(){
   return (
-    <>
+    <div className="main-container">
     "We are in the Main container"
-    </>
+    </div>
   )
 }
 }
