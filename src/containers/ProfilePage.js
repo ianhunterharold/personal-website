045c358ac4420b5a7cbe3e916b6d4771485f06f9
ianhunterharold <ianhunterharold@gmail.com>
2020-06-04 17:22:56 -0600

    wip basic layout almost complete

--- src/containers/ProfilePage.js
+++ src/containers/ProfilePage.js
@@ -12,29 +12,41 @@ class ProfilePage extends Component{
     return(
       <div>
   <Grid  columns={3} divided padded>
+    <Grid.Row centered>
+      <Grid.Column  columns={2} width={10} color={"white"} key={'white'} padded='horizonally'>
+        <Segment>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+        </Segment>
 
+        <Grid.Row colums={3}>
+        
+        <Grid.Column>
+        <Segment>
+          proj1
+        </Segment>
+          </Grid.Column>
+          <Grid.Column>
+        <Segment>
+          pjr2
+        </Segment>
+          </Grid.Column>
+          <Grid.Column>
+        <Segment>
+          prj3
+        </Segment>
+          </Grid.Column>
 
-  <Grid.Row centered>
-    <Grid.Column width={10} color={"orange"} key={'orange'} padded='horizonally'>
-    <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
-    <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
-    <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
-    </Grid.Column>
+        
+        </Grid.Row>
+
+      </Grid.Column>
     
-
-    <Grid.Column color={'black'} key={'black'} padded='vertically'>
-      <Segment>pic,name, quote
-      <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      
-      
-      </Segment>
-    </Grid.Column>
- 
-  </Grid.Row>
-
-
+      <Grid.Column color={'black'} key={'black'} padded='vertically'>
+        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+      </Grid.Column>
+    </Grid.Row>
 
 
   </Grid>
